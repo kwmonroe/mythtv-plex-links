900b0ee5e238f4700851ea6a4fd806cf8b8ac9bb
Kevin W Monroe <kevin.monroe@canonical.com>
2015-04-05 11:11:58 -0500

    get recording data in 1 query, make VARs more descriptive

--- mythpreprocess.sh
+++ mythpreprocess.sh
@@ -7,16 +7,16 @@
 
 # The following values adjust the script parameters:
 #
-# Set this to where the pretty links should reside, making sure to include the
-# trailing /.
-PRETTYDIRNAME="/storage/htpc/recordedtv/"
+# Set this to the directory of the Plex Library where myth recording symlinks
+# should reside.
+PLEXLIBRARYDIR="/mnt/esata/recordedtv"
 # Set this to the URL prefix of your Plex Media Server
-PMSURL="http://192.168.1.200:32400/"
+PMSURL="http://192.168.1.20:32400/"
 # Set this to the section number of your recorded TV shows library. To find
 # this out, go to your plex media server and navigate to the desired library.
 # Look at the URL for that page, and at the end you should see
 # /section/<number>. The number here is your section number.
-PMSSEC="8"
+PMSSEC="6"
 # Set this to the location of the mythtv config.xml file. It's needed to
 # determine the mysql login. If you're running mythbuntu, you shouldn't need to
 # change this.
@@ -29,31 +29,36 @@ CONFIGXML="/home/mythtv/.mythtv/config.xml"
 DBUSER="$(awk -F '[<>]' '/UserName/{print $3}' $CONFIGXML)"
 DBPASS="$(awk -F '[<>]' '/Password/{print $3}' $CONFIGXML)"
 
+# TODO: sanity check values
 CHANID=$1 && STARTTIME=$2
 
 # Populate recording information from sql database
-TITLE=$(mysql mythconverg --user=$DBUSER --password=$DBPASS -se \
-  "SELECT title FROM recorded WHERE chanid=\"$CHANID\" AND starttime=\"$STARTTIME\";")
-SUBTITLE=$(mysql mythconverg --user=$DBUSER --password=$DBPASS -se \
-  "SELECT subtitle FROM recorded WHERE chanid=\"$CHANID\" AND starttime=\"$STARTTIME\";")
-DATE=$(mysql mythconverg --user=$DBUSER --password=$DBPASS -se \
-  "SELECT starttime FROM recorded WHERE chanid=\"$CHANID\" AND starttime=\"$STARTTIME\";")
-FILENAME=$(mysql mythconverg --user=$DBUSER --password=$DBPASS -se \
-  "SELECT basename FROM recorded WHERE chanid=\"$CHANID\" AND starttime=\"$STARTTIME\";")
-STORAGEGROUP=$(mysql mythconverg --user=$DBUSER --password=$DBPASS -se \
-  "SELECT storagegroup FROM recorded WHERE chanid=\"$CHANID\" AND starttime=\"$STARTTIME\";")
-DIRNAME=$(mysql mythconverg --user=$DBUSER --password=$DBPASS -se \
+RECORDING=($(mysql mythconverg --user=$DBUSER --password=$DBPASS -se \
+  "SELECT title, season, episode, basename, storagegroup  FROM recorded WHERE chanid=\"$CHANID\" AND starttime=\"$STARTTIME\";"))
+
+# Set vars from above query results, padding season and episode with 0 if needed
+# TODO: sanity check values
+TITLE=${RECORDING[0]}
+SEASON=`printf "%02d" ${RECORDING[1]}`
+EPISODE=`printf "%02d" ${RECORDING[2]}`
+FILENAME=${RECORDING[3]}
+STORAGEGROUP=${RECORDING[4]}
+
+SGDIR=$(mysql mythconverg --user=$DBUSER --password=$DBPASS -se \
   "SELECT dirname FROM storagegroup WHERE groupname=\"$STORAGEGROUP\";")
-FILEPATH="$DIRNAME$FILENAME"
-PRETTYNAME="$TITLE $SUBTITLE $DATE.mpg"
-PRETTYSUBDIR="$PRETTYDIRNAME$TITLE/"
-PRETTYFILEPATH="$PRETTYSUBDIR$PRETTYNAME"
+
+MYTHFILE="$SGDIR/$FILENAME"
+PLEXFILE="$TITLE - s${SEASON}e${EPISODE} - $STARTTIME.mpg"
+PLEXSHOWDIR="$PLEXLIBRARYDIR/$TITLE"
+PLEXFILEPATH="$PLEXSHOWDIR/$PLEXFILE"
 
 # create pretty name and path for file
-mkdir -p "$PRETTYSUBDIR"
-ln -s "$FILEPATH" "$PRETTYFILEPATH"
-# Prune all dead links and empty folders
-find -L $PRETTYDIRNAME -type l -delete
-find $PRETTYDIRNAME -type d -empty -delete
+mkdir -p "$PLEXSHOWDIR"
+ln -s "$MYTHFILE" "$PLEXFILEPATH"
 
+# Prune all dead links and empty folders
+find -L "$PLEXLIBRARYDIR" -type l -delete
+find "$PLEXLIBRARYDIR" -type d -empty -delete
+
+# Update Plex library
 curl "$PMSURL"library/sections/"$PMSSEC"/refresh
